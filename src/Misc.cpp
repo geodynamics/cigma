263e9fbb9a8af90381bc0ef31f6ad628a883ad84
Luis Armendariz <luis@geodynamics.org>
2007-12-19 20:07:08 +0000

    Add Misc.{h,cpp} for miscellaneous functions

---
+++ src/Misc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+#include "Misc.h"
+
+
+double pick_from_interval(double a, double b)
+{
+    return a + (b - a) * rand()/(RAND_MAX + 1.0);
+}
+
+void bbox_random_point(double minpt[3], double maxpt[3], double x[3])
+{
+    const int nsd = 3;
+    for (int i = 0; i < nsd; i++)
+    {
+        assert(minpt[i] <= maxpt[i]);
+        x[i] = pick_from_interval(minpt[i], maxpt[i]);
+    }
+}
+
---
+++ src/Misc.h
@@ -0,0 +1,10 @@
+#ifndef __MISC_H__
+#define __MISC_H__
+
+#include <cstdlib>
+#include <ctime>
+
+double pick_from_interval(double a, double b);
+void bbox_random_point(double minpt[3], double maxpt[3], double x[3]);
+
+#endif
