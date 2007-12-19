228c309a98fb794cf6a2ac9080ba2794bc9044c8
Luis Armendariz <luis@geodynamics.org>
2007-12-19 20:06:07 +0000

    Class for storing geometric points

---
+++ src/Points.h
@@ -0,0 +1,74 @@
+#ifndef __POINTS_H__
+#define __POINTS_H__
+
+#include <cassert>
+
+namespace cigma
+{
+    class Points;
+}
+
+/**
+ * @brief Array object representing a set of points as a contiguous
+ * memory block (row major storage).
+ *
+ */
+class cigma::Points
+{
+public:
+    Points();
+    ~Points();
+
+public:
+    void set_data(double *data, int num, int dim);
+
+public:
+    double operator()(int i, int j);
+    double *operator[](int i);
+
+public:
+    int n_points() const;
+    int n_dim() const;
+    int index(int i, int j) const;
+
+public:
+    int num;
+    int dim;
+    double *data;
+};
+
+// ---------------------------------------------------------------------------
+
+inline double cigma::Points::operator()(int i, int j)
+{
+    //assert(0 <= i); assert(i < num);
+    //assert(0 <= j); assert(j < dim);
+    return data[index(i,j)];
+}
+
+inline double *cigma::Points::operator[](int i)
+{
+    //assert(0 <= i); assert(i < num);
+    return &data[dim*i];
+}
+
+inline int cigma::Points::n_points() const
+{
+    return num;
+}
+
+inline int cigma::Points::n_dim() const
+{
+    return dim;
+}
+
+inline int cigma::Points::index(int i, int j) const
+{
+    //assert(0 <= i); assert(i < num);
+    //assert(0 <= j); assert(j < dim);
+    return dim*i + j;
+}
+
+// ---------------------------------------------------------------------------
+
+#endif
