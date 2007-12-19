846907f32d0dc1d4c322f6040365c8f9d628a5a1
Luis Armendariz <luis@geodynamics.org>
2007-12-19 20:06:46 +0000

    Updated Field class with another virtual eval() method

--- src/Field.cpp
+++ src/Field.cpp
@@ -1,4 +1,16 @@
+#include <cassert>
 #include "Field.h"
 
 cigma::Field::~Field() {};
+
+void cigma::Field::eval(Points &domain, Points &range)
+{
+    assert(domain.n_points() == range.n_points());
+    for (int i = 0; i < domain.n_points(); i++)
+    {
+        double *x = domain[i];
+        double *y = range[i];
+        eval(x,y);
+    }
+}
 
--- src/Field.h
+++ src/Field.h
@@ -1,20 +1,27 @@
 #ifndef __FIELD_H__
 #define __FIELD_H__
 
+#include "Points.h"
+
+
 namespace cigma
 {
     class Field;
 }
 
-
-/*
- * Base Field object
+
+/**
+ * @brief Base Field object
  */
 class cigma::Field
 {
 public:
+    virtual int n_dim() = 0;
+    virtual int n_rank() = 0;
     virtual void eval(double *point, double *value) = 0;
+    virtual void eval(Points &domain, Points &range);
     virtual ~Field();
 };
+
 
 #endif
