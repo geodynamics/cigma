b501c060d7c9b9a4501be27bba4a99a506ffb3f2
Luis Armendariz <luis@geodynamics.org>
2008-01-15 05:28:23 +0000

    Base class for writers

---
+++ src/Writer.h
@@ -0,0 +1,21 @@
+#ifndef __WRITER_H__
+#define __WRITER_H__
+
+#include <cstdio>
+#include <string>
+
+namespace cigma
+{
+    class Writer;
+};
+
+class cigma::Writer
+{
+public:
+    Writer();
+    ~Writer();
+};
+
+// ---------------------------------------------------------------------------
+
+#endif
