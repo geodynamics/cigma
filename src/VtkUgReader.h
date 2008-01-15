8db318413e0178968833b7b9544ac5f42a68d490
Luis Armendariz <luis@geodynamics.org>
2008-01-15 05:28:17 +0000

    Small change to VtkUgReader.h

--- src/VtkUgReader.h
+++ src/VtkUgReader.h
@@ -20,8 +20,8 @@ public:
     void open(std::string filename);
 
 public:
-    void get_connectivity(int **connectivity, int *nel, int *ndofs);
     void get_coordinates(double **coordinates, int *nno, int *nsd);
+    void get_connectivity(int **connectivity, int *nel, int *ndofs);
     void get_vector_point_data(const char *name, double **vectors, int *num, int *dim);
     void get_scalar_point_data(const char *name, double **scalars, int *num, int *dim);
 
