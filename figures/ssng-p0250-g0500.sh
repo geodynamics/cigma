44550de007a6cbef165045596bbf40fd084bc7be
Luis Armendariz <luis>
2008-02-06 21:06:08 +0000

    Updated script for visualization chapter figures.

--- figures/ssng-p0250-g0500.sh
+++ figures/ssng-p0250-g0500.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 bm=bmssnog
-bmfile=$bm.h5
+bmfile=$HOME/benchmarks/$bm.h5
 prefix=ssng-p0250-g0500
 
 #########
 
 echo "---------------------------------------------"
 var=variables/displacement/step00000
-cigma-compare --first=$bmfile:/pylith/${bm}_tet4_0250m/$var     \
+cigma compare --first=$bmfile:/pylith/${bm}_tet4_0250m/$var     \
               --second=$bmfile:/geofest/${bm}_tet4_0500m/$var   \
-              --output=$prefix.disp-000.vtk 
+              --first-mesh=$bmfile:/mesh/${bm}_tet4_0250m       \
+              --second-mesh=$bmfile:/mesh/${bm}_tet4_0500m      \
+              --verbose --output-frequency=10000                \
+              --output=$prefix.disp-000.vtk
 
 
 #########
@@ -19,7 +22,10 @@ echo "---------------------------------------------"
 t=step00000
 var1=variables/velocity/$t
 var2=variables/del_displacement/$t
-cigma-compare --first=$bmfile:/pylith/${bm}_tet4_0250m/$var1    \
+cigma compare --first=$bmfile:/pylith/${bm}_tet4_0250m/$var1    \
               --second=$bmfile:/geofest/${bm}_tet4_0500m/$var2  \
+              --first-mesh=$bmfile:/mesh/${bm}_tet4_0250m       \
+              --second-mesh=$bmfile:/mesh/${bm}_tet4_0500m      \
+              --verbose --output-frequency=10000                \
               --output=$prefix.velo-000.vtk 
 
