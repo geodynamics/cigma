0b1c6afb3ec760304c13835fda0bef7e579282c4
Luis Armendariz <luis@geodynamics.org>
2008-03-12 18:03:04 +0000

    Testing uploading from remote repository  * Moved utility programs into /scratch/utils/ directory  * Removed utilities from /src/ directory

---
+++ scratch/utils/rebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -v
+cd .. && make dist && cd - && \
+rm -rf cigma-0.9.1 && \
+tar xvzf cigma-0.9.1.tar.gz && \
+cd cigma-0.9.1 && \
+./configure --prefix=$HOME/cigma/build/cigma-0.9.1/opt \
+            --with-hdf5=$HOME/opt/hdf5/1.6.5 && \
+make && make install
