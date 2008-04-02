84011e6a2f94d86bdde1b3a0d198e1aa1dfe6df4
Luis Armendariz <luis@geodynamics.org>
2008-04-02 18:01:58 +0000

    Fixes for PointField::eval()

--- src/PointField.cpp
+++ src/PointField.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 #include <cassert>
 #include "PointField.h"
 
+using namespace std;
 using namespace cigma;
 
 // ---------------------------------------------------------------------------
@@ -34,26 +36,47 @@ void PointField::set_values(double *vals, int nvals, int rank)
 
 bool PointField::eval(double *point, double *value)
 {
-    /* XXX: quick sanity check
-    static int checked = 0;
-    if (!checked)
-    {
-        assert(points->n_points() != 0);
-        assert(values->n_points() != 0);
-        assert(points->n_points() == values->n_points());
-        assert(points->n_dim() == values->n_dim());
-        checked = 1;
-    } // */
+    const bool debug = false;
 
+    // XXX: sanity check
+    //static int checked = 0;
+    //if (!checked)
+    //{
+    //    assert(points->n_points() != 0);
+    //    assert(values->n_points() != 0);
+    //    assert(points->n_points() == values->n_points());
+    //    assert(points->n_dim() == values->n_dim());
+    //    checked = 1;
+    //}
 
     // Find index of closest point
     int n;
     bool found = points->find_ann_index(point, &n);
+    if (!found) { return false; }
 
-    if (found && (0 <= n) && (n < points->n_points()))
+    if ((0 <= n) && (n < points->n_points()))
     {
+        int i;
+
+        // Show ANN point
+        if (debug)
+        {
+            double *p = (*points)[n];
+
+            cerr << "ANNPT( ";
+            for (i = 0; i < points->n_dim(); i++)
+            {
+                cerr << p[i] << " ";
+            }
+            cerr << ")" << endl;
+        }
+
         // Retrieve corresponding value
-        value = (*values)[n];
+        double *v = (*values)[n];
+        for (i = 0; i < values->n_dim(); i++)
+        {
+            value[i] = v[i];
+        }
         return true;
     }
 
