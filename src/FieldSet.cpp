8240b0d30fcd08082c7b6e390b04b57a5b7a460e
Luis Armendariz <luis@geodynamics.org>
2008-04-09 00:35:29 +0000

    Update set of pre-defined fields.

--- src/FieldSet.cpp
+++ src/FieldSet.cpp
@@ -4,6 +4,8 @@
 #include "FieldSet.h"
 #include "ZeroField.h"
 #include "GaleBenchmarkFields.h"
+#include "OkadaBenchmarkFields.h"
+#include "TestBenchmarkFields.h"
 
 using namespace std;
 using namespace cigma;
@@ -12,17 +14,32 @@ using namespace cigma;
 
 //
 // This method will initialize our FieldSet container with a
-// number of predefined fields. Others may be added
+// number of predefined fields. Others fields may be added
 //
 void FieldSet::initialize()
 {
-
+    //
+    // XXX: In retrospect, we need to modify UserField to implement
+    // lazy loading (do not initialize in constructor).
+    //
     ZeroField *zero = new ZeroField();
     addField("zero", zero);
+
+    typedef okada::strike_slip::Disloc3d OkadaStrikeSlip;
+    OkadaStrikeSlip *disloc3d = new OkadaStrikeSlip();
+    addField("disloc3d", disloc3d);
 
     typedef gale::circular_inclusion::Pressure GCIPressure;
     GCIPressure *pressure = new GCIPressure();
     addField("gale.circular_inclusion.pressure", pressure);
+
+    typedef cigma::test_benchmarks::Cube CubeBM;
+    CubeBM *cube = new CubeBM();
+    addField("test.cube", cube);
+
+    typedef cigma::test_benchmarks::Square SquareBM;
+    SquareBM *square = new SquareBM();
+    addField("test.square", square);
 }
 
 
